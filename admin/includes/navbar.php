9927555f3b30fff7d5b63a07c974ce7eb13ba3d7
Mavine1 <mavinemalin@gmail.com>
2025-05-30 00:40:02 +0300

    your energy choose beneath end

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,52 +1,54 @@
-<header class="main-header" style="background-color:#717A83 " >
-  <!-- Logo -->
-  <a href="#" class="logo " style="background-color: #717A83">
-    <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini" style="background-color: #717A83 "><b>O</b>VS</span>
-    <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg " style="background-color:#717A83 ;color:white ; font-size: 22px; font-family:Times  "><marquee behavior="scroll" direction="left">Online Voting System</marquee></span>
-    
-
-  </a>
-  <!-- Header Navbar: style can be found in header.less -->
-  <nav class="navbar navbar-static-top " style="background-color: #717A83 ">
+<header class="main-header" style="background: linear-gradient(135deg, #1e40af 0%, #3b82f6 50%, #ef4444 100%);">   
+  <!-- Logo -->   
+  <a href="#" class="logo" style="background: linear-gradient(135deg, #1e40af 0%, #2563eb 100%);">     
+    <!-- mini logo for sidebar mini 50x50 pixels -->     
+    <span class="logo-mini" style="background: linear-gradient(135deg, #1e40af 0%, #2563eb 100%);"><b>O</b>VS</span>     
+    <!-- logo for regular state and mobile devices -->     
+    <span class="logo-lg" style="background: linear-gradient(135deg, #1e40af 0%, #2563eb 100%); color: #ffffff; font-size: 22px; font-family: Times;">
+      <marquee behavior="scroll" direction="left">Online Voting System</marquee>
+    </span>         
+  </a>   
   
+  <!-- Header Navbar: style can be found in header.less -->   
+  <nav class="navbar navbar-static-top" style="background: linear-gradient(135deg, #1e40af 0%, #3b82f6 50%, #ef4444 100%);">         
+    <!-- Sidebar toggle button-->     
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">       
+      <span class="sr-only">Toggle navigation</span>     
+    </a>      
+    
+    <div class="navbar-custom-menu" style="color: #ffffff; font-size: 17px; font-family: Times;">       
+      <ul class="nav navbar-nav" style="background: linear-gradient(135deg, #1e40af 0%, #3b82f6 50%, #ef4444 100%); color: #ffffff; font-size: 17px; font-family: Times;">         
+        <!-- User Account: style can be found in dropdown.less -->         
+        <li class="dropdown user user-menu" style="color: #ffffff;">           
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="background: linear-gradient(135deg, #1e40af 0%, #2563eb 100%); color: #ffffff; font-size: 17px; font-family: Times;">             
+            <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="user-image" alt="User Image">             
+            <span class="hidden-xs"><?php echo $user['firstname'].' '.$user['lastname']; ?></span>           
+          </a>           
+          
+          <ul class="dropdown-menu" style="background: linear-gradient(135deg, #1e40af 0%, #ef4444 100%); color: #ffffff; font-size: 17px; font-family: Times;">             
+            <!-- User image -->             
+            <li class="user-header" style="background: linear-gradient(135deg, #1e40af 0%, #ef4444 100%); color: #ffffff; font-size: 17px; font-family: Times;">               
+              <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">                
+              <p>                 
+                <?php echo $user['firstname'].' '.$user['lastname']; ?>                 
+                <small>Member since <?php echo date('M. Y', strtotime($user['created_on'])); ?></small>               
+              </p>             
+            </li>             
+            
+            <li class="user-footer" style="background: linear-gradient(135deg, #2563eb 0%, #dc2626 100%); color: #ffffff; font-size: 17px; font-family: Times;">               
+              <div class="pull-left" style="background: transparent; color: #ffffff; font-size: 17px; font-family: Times;">                 
+                <a href="#profile" data-toggle="modal" class="btn btn-default btn-curve" style="background-color: #ffffff; color: #1e40af; border: 2px solid #1e40af;" id="admin_profile">Update</a>               
+              </div>               
+              
+              <div class="pull-right" style="background: transparent; color: #ffffff; font-size: 17px; font-family: Times;">                 
+                <a href="logout.php" class="btn btn-default btn-curve" style="background-color: #ffffff; color: #ef4444; border: 2px solid #ef4444;">Sign out</a>               
+              </div>             
+            </li>           
+          </ul>         
+        </li>       
+      </ul>     
+    </div>   
+  </nav> 
+</header> 
 
-    <!-- Sidebar toggle button-->
-    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-      <span class="sr-only ">Toggle navigation</span>
-    </a>
-
-    <div class="navbar-custom-menu" style="color:black ; font-size: 17px; font-family:Times">
-      <ul class="nav navbar-nav" style="background-color: #717A83 ;color:black ; font-size: 17px; font-family:Times" >
-        <!-- User Account: style can be found in dropdown.less -->
-        <li class="dropdown user user-menu  " style="color:black">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="background-color: #717A83 ;color:white ; font-size: 17px; font-family:Times" >
-            <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="user-image" alt="User Image">
-            <span class="hidden-xs"><?php echo $user['firstname'].' '.$user['lastname']; ?></span>
-          </a>
-          <ul class="dropdown-menu" style="background-color: #4d5863 ;color:black ; font-size: 17px; font-family:Times">
-            <!-- User image -->
-            <li class="user-header" style="background-color:#4d5863 ;color:black ; font-size: 17px; font-family:Times">
-              <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
-
-              <p>
-                <?php echo $user['firstname'].' '.$user['lastname']; ?>
-                <small>Member since <?php echo date('M. Y', strtotime($user['created_on'])); ?></small>
-              </p>
-            </li>
-            <li class="user-footer" style="background-color: #90979e ;color:white ; font-size: 17px; font-family:Times" >
-              <div class="pull-left " style="background-color: #90979e ;color:white ; font-size: 17px; font-family:Times">
-                <a href="#profile" data-toggle="modal" class="btn btn-default btn-curve" style="background-color: #d2d5d8" id="admin_profile">Update</a>
-              </div>
-              <div class="pull-right" style="background-color:#90979e ;color:black ; font-size: 17px; font-family:Times">
-                <a href="logout.php" class="btn btn-default btn-curve" style="background-color: #d2d5d8" >  Sign out</a>
-              </div>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </nav>
-</header>
 <?php include 'includes/profile_modal.php'; ?>
