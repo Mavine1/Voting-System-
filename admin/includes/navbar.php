1fd9f37781e760546b4923c1adf4976044f54989
Mavine1 <mavinemalin@gmail.com>
2025-05-30 00:32:01 +0300

    rear saddle land wall clearly

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,16 +1,18 @@
-<header class="main-header" style="background-color:#1e40af" >
+<header class="main-header" style="background-color:#1e40af">
   <!-- Logo -->
   <a href="#" class="logo " style="background-color: #1e40af">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini" style="background-color: #1e40af "><b>O</b>VS</span>
     <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg " style="background-color:#1e40af ;color:white ; font-size: 22px; font-family:Times  "><marquee behavior="scroll" direction="left">Online Voting System</marquee></span>
-    
+    <span class="logo-lg " style="background-color:#1e40af ;color:white ; font-size: 22px; font-family:Times  ">
+      <marquee behavior="scroll" direction="left">Online Voting System</marquee>
+    </span>
+
 
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top " style="background-color: #1e40af ">
-  
+
 
     <!-- Sidebar toggle button-->
     <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
@@ -18,29 +20,30 @@
     </a>
 
     <div class="navbar-custom-menu" style="color:black ; font-size: 17px; font-family:Times">
-      <ul class="nav navbar-nav" style="background-color: #1e40af ;color:black ; font-size: 17px; font-family:Times" >
+      <ul class="nav navbar-nav" style="background-color: #1e40af ;color:black ; font-size: 17px; font-family:Times">
         <!-- User Account: style can be found in dropdown.less -->
         <li class="dropdown user user-menu  " style="color:black">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="background-color: #1e40af ;color:white ; font-size: 17px; font-family:Times" >
-            <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="user-image" alt="User Image">
-            <span class="hidden-xs"><?php echo $user['firstname'].' '.$user['lastname']; ?></span>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="background-color: #1e40af ;color:white ; font-size: 17px; font-family:Times">
+            <img src="<?php echo (!empty($user['photo'])) ? '../images/' . $user['photo'] : '../images/profile.jpg'; ?>" class="user-image" alt="User Image">
+            <span class="hidden-xs"><?php echo $user['firstname'] . ' ' . $user['lastname']; ?></span>
           </a>
           <ul class="dropdown-menu" style="background-color: #ffffff ;color:black ; font-size: 17px; font-family:Times">
             <!-- User image -->
             <li class="user-header" style="background-color:#ffffff ;color:black ; font-size: 17px; font-family:Times">
-              <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
+              <img src="<?php echo (!empty($user['photo'])) ? '../images/' . $user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
 
               <p>
-                <?php echo $user['firstname'].' '.$user['lastname']; ?>
-                <small>Member since <?php echo date('M. Y', strtotime($user['created_on'])); ?></small>
+                <?php echo $user['firstname'] . ' ' . $user['lastname']; ?>
+                <small>Member since <?php echo date('Y'); ?></small>
               </p>
+
             </li>
-            <li class="user-footer" style="background-color: #ffffff ;color:black ; font-size: 17px; font-family:Times" >
+            <li class="user-footer" style="background-color: #ffffff ;color:black ; font-size: 17px; font-family:Times">
               <div class="pull-left " style="background-color: #ffffff ;color:black ; font-size: 17px; font-family:Times">
                 <a href="#profile" data-toggle="modal" class="btn btn-default btn-curve" style="background-color: #ef4444; color: white;" id="admin_profile">Update</a>
               </div>
               <div class="pull-right" style="background-color:#ffffff ;color:black ; font-size: 17px; font-family:Times">
-                <a href="logout.php" class="btn btn-default btn-curve" style="background-color: #ef4444; color: white;" >  Sign out</a>
+                <a href="logout.php" class="btn btn-default btn-curve" style="background-color: #ef4444; color: white;"> Sign out</a>
               </div>
             </li>
           </ul>
