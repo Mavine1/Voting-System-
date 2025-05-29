6576fa43e95076b3e5fdb0939cdc80dc730a0c43
Mavine1 <mavinemalin@gmail.com>
2025-05-30 01:50:01 +0300

    scared tower finest accident force

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,12 @@
-<header class="main-header" style="background-color:#717A83 ">
-  <nav class="navbar navbar-static-top" style="background-color:#717A83 ">
-    <div class="container" style="background-color:#717A83 ">
-      <div class="navbar-header" style="background-color:#717A83 ">
-        <a href="#" class="navbar-brand" style="background-color:#717A83 ;color:black ; font-size: 22px; font-family:Times  "><b>ONLINE<b> VOTING</b> SYSTEM</a>
-        <button type="button" class="navbar-toggle collapsed"style="background-color:#717A83 " data-toggle="collapse" data-target="#navbar-collapse">
-          <i class="fa fa-bars"></i>
+<header class="main-header" style="background-color: #1e40af;">
+  <nav class="navbar navbar-static-top" style="background-color: #1e40af;">
+    <div class="container" style="background-color: #1e40af;">
+      <div class="navbar-header" style="background-color: #1e40af;">
+        <a href="#" class="navbar-brand" style="background-color: #1e40af; color: #ffffff; font-size: 22px; font-family: Times; text-decoration: none;">
+          <b>ONLINE <b>VOTING</b> SYSTEM</b>
+        </a>
+        <button type="button" class="navbar-toggle collapsed" style="background-color: #1e40af; border: 2px solid #ffffff;" data-toggle="collapse" data-target="#navbar-collapse">
+          <i class="fa fa-bars" style="color: #ffffff;"></i>
         </button>
       </div>
 
@@ -14,8 +16,8 @@
           <?php
             if(isset($_SESSION['student'])){
               echo "
-                <li><a href='index.php'>HOME</a></li>
-                <li><a href='transaction.php'>TRANSACTION</a></li>
+                <li><a href='index.php' style='color: #ffffff; font-family: Times; font-size: 16px; padding: 15px 20px;'>HOME</a></li>
+                <li><a href='transaction.php' style='color: #ffffff; font-family: Times; font-size: 16px; padding: 15px 20px;'>TRANSACTION</a></li>
               ";
             } 
           ?>
@@ -23,15 +25,22 @@
       </div>
       <!-- /.navbar-collapse -->
       <!-- Navbar Right Menu -->
-      <div class="navbar-custom-menu"  >
-        <ul class="nav navbar-nav"  >
-          <li class="user user-menu" >
-            <a href="">
-              <img src="<?php echo (!empty($voter['photo'])) ? 'images/'.$voter['photo'] : 'images/profile.jpg' ?>" class="user-image" alt="User Image">
-              <span class="hidden-xs"  style="color:black ; font-size: 22px; font-family:Times  "><?php echo $voter['firstname'].' '.$voter['lastname']; ?></span>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="user user-menu">
+            <a href="" style="color: #ffffff; padding: 10px 15px;">
+              <img src="<?php echo (!empty($voter['photo'])) ? 'images/'.$voter['photo'] : 'images/profile.jpg' ?>" class="user-image" alt="User Image" style="border: 2px solid #ffffff; border-radius: 50%;">
+              <span class="hidden-xs" style="color: #ffffff; font-size: 18px; font-family: Times; margin-left: 10px;">
+                <?php echo $voter['firstname'].' '.$voter['lastname']; ?>
+              </span>
             </a>
           </li>
-          <li><a href="logout.php"> <b style="color:black ; font-size: 22px;> <i class=fa fa-sign-out"> </b></i> <b style="color:black ; font-size: 22px; font-family:Times  " > LOGOUT </b></a></li>  
+          <li>
+            <a href="logout.php" style="color: #ffffff; padding: 15px 20px; text-decoration: none;">
+              <i class="fa fa-sign-out" style="color: #ffffff; font-size: 18px; margin-right: 5px;"></i>
+              <b style="color: #ffffff; font-size: 18px; font-family: Times;">LOGOUT</b>
+            </a>
+          </li>  
         </ul>
       </div>
       <!-- /.navbar-custom-menu -->
@@ -39,3 +48,27 @@
     <!-- /.container-fluid -->
   </nav>
 </header>
+
+<style>
+/* Additional CSS for hover effects and better styling */
+.navbar-nav > li > a:hover {
+    background-color: rgba(255, 255, 255, 0.1) !important;
+    color: #ffffff !important;
+}
+
+.navbar-brand:hover {
+    background-color: rgba(255, 255, 255, 0.1) !important;
+    color: #ffffff !important;
+}
+
+.user-image {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    margin-right: 10px;
+}
+
+.navbar-toggle:hover {
+    background-color: rgba(255, 255, 255, 0.1) !important;
+}
+</style>
