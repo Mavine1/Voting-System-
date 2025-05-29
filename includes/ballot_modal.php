7a1f44fca61f60107480e4e47ac7a17942db39ef
Mavine1 <mavinemalin@gmail.com>
2025-05-30 01:48:00 +0300

    unusual cage perhaps put fifty

--- includes/ballot_modal.php
+++ includes/ballot_modal.php
@@ -1,17 +1,20 @@
 <!-- Preview -->
 <div class="modal fade" id="preview_modal">
     <div class="modal-dialog">
-        <div class="modal-content" style="background-color: #d8d1bd ;color:black ; font-size: 15px; font-family:Times" >
-            <div class="modal-header">
-              <button type="button"  class=" btn btn-close btn-curve pull-right" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" style="color:black ; font-size: 15px; font-family:Times">Vote Preview</h4>
+        <div class="modal-content" style="background-color: #ffffff; color: #1e40af; font-size: 15px; font-family: Times;">
+            <div class="modal-header" style="border-bottom: 2px solid #1e40af;">
+                <button type="button" class="btn btn-close btn-curve pull-right" data-dismiss="modal" aria-label="Close" style="background-color: #ef4444; color: #ffffff; border: none;">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" style="color: #1e40af; font-size: 18px; font-family: Times; font-weight: bold;">Vote Preview</h4>
             </div>
             <div class="modal-body">
-              <div id="preview_body"></div>
+                <div id="preview_body"></div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-curve pull-left" style='background-color:  #FFDEAD  ;color:black ; font-size: 12px; font-family:Times' data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+            <div class="modal-footer" style="border-top: 2px solid #1e40af;">
+                <button type="button" class="btn btn-default btn-curve pull-left" style="background-color: #1e40af; color: #ffffff; font-size: 12px; font-family: Times; border: none;" data-dismiss="modal">
+                    <i class="fa fa-close"></i> Close
+                </button>
             </div>
         </div>
     </div>
@@ -20,17 +23,22 @@
 <!-- Platform -->
 <div class="modal fade" id="platform">
     <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"><b><span class="candidate"></b></h4>
+        <div class="modal-content" style="background-color: #ffffff; color: #1e40af;">
+            <div class="modal-header" style="border-bottom: 2px solid #1e40af;">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="background-color: #ef4444; color: #ffffff; border: none; padding: 8px 12px;">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" style="color: #1e40af; font-weight: bold;">
+                    <b><span class="candidate"></span></b>
+                </h4>
             </div>
             <div class="modal-body">
-              <p id="plat_view"></p>
+                <p id="plat_view" style="color: #1e40af;"></p>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+            <div class="modal-footer" style="border-top: 2px solid #1e40af;">
+                <button type="button" class="btn btn-default btn-flat pull-left" style="background-color: #1e40af; color: #ffffff; border: none;" data-dismiss="modal">
+                    <i class="fa fa-close"></i> Close
+                </button>
             </div>
         </div>
     </div>
@@ -39,29 +47,34 @@
 <!-- View Ballot -->
 <div class="modal fade" id="view">
     <div class="modal-dialog">
-        <div class="modal-content" style="background-color: #d8d1bd ;color:black ; font-size: 15px; font-family:Times ">
-            <div class="modal-header">
-              <button type="button" class=" btn btn-close btn-curve pull-right" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title"> <b>Your Votes </b></h4>
+        <div class="modal-content" style="background-color: #ffffff; color: #1e40af; font-size: 15px; font-family: Times;">
+            <div class="modal-header" style="border-bottom: 2px solid #1e40af;">
+                <button type="button" class="btn btn-close btn-curve pull-right" data-dismiss="modal" aria-label="Close" style="background-color: #ef4444; color: #ffffff; border: none;">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title" style="color: #1e40af; font-weight: bold; font-size: 18px;">
+                    <b>Your Votes</b>
+                </h4>
             </div>
             <div class="modal-body">
-              <?php
-                $id = $voter['id'];
-                $sql = "SELECT *, candidates.firstname AS canfirst, candidates.lastname AS canlast FROM votes LEFT JOIN candidates ON candidates.id=votes.candidate_id LEFT JOIN positions ON positions.id=votes.position_id WHERE voters_id = '$id'  ORDER BY positions.priority ASC";
-                $query = $conn->query($sql);
-                while($row = $query->fetch_assoc()){
-                  echo "
-                    <div class='row votelist'>
-                      <span class='col-sm-4'><span class='pull-right'><b>".$row['description']." :</b></span></span> 
-                      <span class='col-sm-8'>".$row['canfirst']." ".$row['canlast']."</span>
-                    </div>
-                  ";
-                }
-              ?>
+                <?php
+                    $id = $voter['id'];
+                    $sql = "SELECT *, candidates.firstname AS canfirst, candidates.lastname AS canlast FROM votes LEFT JOIN candidates ON candidates.id=votes.candidate_id LEFT JOIN positions ON positions.id=votes.position_id WHERE voters_id = '$id' ORDER BY positions.priority ASC";
+                    $query = $conn->query($sql);
+                    while($row = $query->fetch_assoc()){
+                        echo "
+                            <div class='row votelist' style='margin-bottom: 10px; padding: 8px; border-bottom: 1px solid #1e40af;'>
+                                <span class='col-sm-4'><span class='pull-right' style='color: #1e40af; font-weight: bold;'><b>".$row['description']." :</b></span></span> 
+                                <span class='col-sm-8' style='color: #1e40af;'>".$row['canfirst']." ".$row['canlast']."</span>
+                            </div>
+                        ";
+                    }
+                ?>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-curve pull-left" style='background-color:  #FFDEAD  ;color:black ; font-size: 12px; font-family:Times' data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+            <div class="modal-footer" style="border-top: 2px solid #1e40af;">
+                <button type="button" class="btn btn-default btn-curve pull-left" style="background-color: #1e40af; color: #ffffff; font-size: 12px; font-family: Times; border: none;" data-dismiss="modal">
+                    <i class="fa fa-close"></i> Close
+                </button>
             </div>
         </div>
     </div>
